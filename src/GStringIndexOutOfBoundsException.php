d02ea3384e7c0df92892c868f258d246f64d48d7
Stephen J. Carnam <steve@steveorevo.com>
2016-05-11 13:05:40 -0700

    Update GStringIndexOutOfBoundsException.php

--- src/GStringIndexOutOfBoundsException.php
+++ src/GStringIndexOutOfBoundsException.php
@@ -6,7 +6,7 @@ namespace Steveorevo;
  * Thrown by GString methods to indicate that an index is either negative or
  * greater than the size of the string.
  */
-class GStringIndexOutOfBoundsException extends Exception {
+class GStringIndexOutOfBoundsException extends \Exception {
 	/**
 	 * Constructs a new GStringIndexOutOfBoundsException class with an argument
 	 * indicating the illegal index.
